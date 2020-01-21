2b45b0e1fc0481321361bfb7d4499eb29920f292
Holly Springsteen <hhspringsteen@gmail.com>
2020-01-21 17:55:20 -0600

    update docs & logos

--- src/stories/0-Welcome.stories.js
+++ src/stories/0-Welcome.stories.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Storybook
 
 // Components
 import { StoryArticle, StoryHeader, StorySection } from '../utils/storybook/StyledStoryComponents';
-import ArmoryLogo from '../icons/armory/armory-logo.svg';
+import ArmoryPreview from '../icons/armory/armory-preview.svg';
 
 // Utils
 import { createStory } from '../utils/storybook/storybookUtils';
 
+const StyledStoryHeader = styled(StoryHeader)`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  > img {
+    height: 300px;
+    max-width: 80%;
+    width: auto;
+  }
+`;
+
 createStory('Intro|/').add('Welcome', () => {
   return (
     <StoryArticle>
-      <StoryHeader>
-        <img src={ArmoryLogo} height="150" alt="armory" />
-      </StoryHeader>
+      <StyledStoryHeader>
+        <img src={ArmoryPreview} alt="armory" />
+      </StyledStoryHeader>
 
       <StorySection>
         <StoryHeader>
